97d8d6219ff0831379dfd68662be17b5bbff85a9
unknown <sinha.ak08@gmail.com>
2023-04-25 19:51:40 +0530

    summary and preview added

--- src/Components/TextBox.js
+++ src/Components/TextBox.js
@@ -9,6 +9,10 @@ export default function TextBox(props) {
     console.log("text changed to uppercase");
   };
 
+  const handleLoClick = () => {
+    setText(text.toLowerCase());
+  };
+
   const textBoxClick = () => {
     if (text==="Enter Text Here")
     setText("");
@@ -18,6 +22,7 @@ export default function TextBox(props) {
     setText(event.target.value)
   }
   return (
+    <>
     <div>
       <h2>{props.heading}</h2>
       <div className="mb-3">
@@ -30,9 +35,20 @@ export default function TextBox(props) {
           rows="7"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
+      <button className="btn btn-primary mx-1" onClick={handleUpClick}>
         Convert to UpperCase
       </button>
+      <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+        Convert to LowerCase
+      </button>
     </div>
+    <div className="container my-2">
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} words & {text.length} characters</p>
+        <p>{0.008*text.split(" ").length} mins to read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+    </div>
+    </>
   );
 }
